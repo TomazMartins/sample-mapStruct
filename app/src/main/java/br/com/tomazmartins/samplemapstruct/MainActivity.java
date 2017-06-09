ba59a04ba7f8160f716f69c3620fb27666bc6a5e
TomazMartins <tomaz.r.martins@gmail.com>
2017-06-09 00:45:03 -0300

    Testing mapStruct and compile
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- app/src/main/java/br/com/tomazmartins/samplemapstruct/MainActivity.java
+++ app/src/main/java/br/com/tomazmartins/samplemapstruct/MainActivity.java
@@ -2,6 +2,15 @@ package br.com.tomazmartins.samplemapstruct;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import br.com.tomazmartins.samplemapstruct.entity.HouseEntity;
+import br.com.tomazmartins.samplemapstruct.entity.PersonEntity;
+import br.com.tomazmartins.samplemapstruct.entity.PhoneEntity;
+import br.com.tomazmartins.samplemapstruct.mapper.PersonMapper;
+import br.com.tomazmartins.samplemapstruct.model.House;
+import br.com.tomazmartins.samplemapstruct.model.Person;
+import br.com.tomazmartins.samplemapstruct.model.Phone;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,62 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        PersonEntity personEntity = new PersonEntity();
+        personEntity.setName( "Tested Person" );
+        personEntity.setEmail( "example@sample.com" );
+
+        PhoneEntity phoneEntity = new PhoneEntity();
+        phoneEntity.setNumber( "99999-9999" );
+        phoneEntity.setOwner( personEntity );
+
+        personEntity.setPhone( phoneEntity );
+
+        HouseEntity houseEntity = new HouseEntity();
+        houseEntity.setAddress( "some place in the World" );
+        houseEntity.setNumberOfRooms( 6 );
+        houseEntity.setOwner( personEntity );
+
+        personEntity.setHouse( houseEntity );
+
+        Person person = PersonMapper.INSTANCE.mapFrom( personEntity );
+
+        StringBuilder builder = new StringBuilder();
+        builder.append( "The SOURCE PersonEntity is:\n" )
+                .append( "Name: " )
+                .append( personEntity.getName() )
+                .append( "\n" )
+                .append( "Email: " )
+                .append( personEntity.getEmail() )
+                .append( "\n" )
+                .append( "Phone: " )
+                .append( personEntity.getPhone().getNumber() )
+                .append( "\n" )
+                .append( "House" )
+                .append( "[" )
+                .append( personEntity.getHouse().getAddress() )
+                .append( ", " )
+                .append( personEntity.getHouse().getNumberOfRooms() )
+                .append( "];" )
+                .append( "\n\n" )
+                .append( "The TARGET Person is:\n" )
+                .append( "Name: " )
+                .append( person.getName() )
+                .append( "\n" )
+                .append( "Email: " )
+                .append( person.getEmail() )
+                .append( "\n" )
+                .append( "Phone: " )
+                .append( person.getPhone().getNumber() )
+                .append( "\n" )
+                .append( "House" )
+                .append( "[" )
+                .append( person.getHouse().getAddress() )
+                .append( ", " )
+                .append( person.getHouse().getNumberOfRooms() )
+                .append( "];" )
+                .append( "\n\n" );
+
+        Log.i( "TEST MapStruct", builder.toString() );
     }
 }
