803b4795a4a8eddab369f1ccdbf5ba871cc79d63
TomazMartins <tomaz.r.martins@gmail.com>
2017-06-09 00:17:20 -0300

    Improving mappers (Invoke another mappers)
    Signed-off-by: TomazMartins <tomaz.r.martins@gmail.com>

--- app/src/main/java/br/com/tomazmartins/samplemapstruct/mapper/PersonMapper.java
+++ app/src/main/java/br/com/tomazmartins/samplemapstruct/mapper/PersonMapper.java
@@ -3,18 +3,12 @@ package br.com.tomazmartins.samplemapstruct.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-import br.com.tomazmartins.samplemapstruct.entity.HouseEntity;
 import br.com.tomazmartins.samplemapstruct.entity.PersonEntity;
-import br.com.tomazmartins.samplemapstruct.entity.PhoneEntity;
-import br.com.tomazmartins.samplemapstruct.model.House;
 import br.com.tomazmartins.samplemapstruct.model.Person;
-import br.com.tomazmartins.samplemapstruct.model.Phone;
 
-@Mapper
+@Mapper( uses = {HouseMapper.class, PhoneMapper.class} )
 public interface PersonMapper {
     PersonMapper INSTANCE = Mappers.getMapper( PersonMapper.class );
 
     Person mapFrom( PersonEntity entity );
-    House mapFrom( HouseEntity entity );
-    Phone mapFrom( PhoneEntity entity );
 }
